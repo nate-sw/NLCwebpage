c2bef4db14a759c11fd52090e35f93e7287b7f6f
nate-sw <105872836+nate-sw@users.noreply.github.com>
2022-09-22 14:45:02 -0400

    Create dump.php
    testing php

---
+++ dump.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Dump</title>
+    </head>
+    <body>
+        <h1>Data Dump</h1>
+        <h2>This is a test</h2>
+        <p><?= var_dump($_SERVER) ?></p>
+    </body>
+
+</html>
