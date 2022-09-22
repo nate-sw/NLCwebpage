645833e24121a444c27e28e6d7f05bb69fb8c13c
nate-sw <105872836+nate-sw@users.noreply.github.com>
2022-09-22 15:03:02 -0400

    Update dump.php

--- dump.php
+++ dump.php
@@ -6,7 +6,7 @@
     <body>
         <h1>Data Dump</h1>
         <h2>This is a test</h2>
-        <p><?= var_dump($_SERVER) ?></p>
+        <p><?= var_dump($_SERVER['HTTP_USER_AGENT']) ?></p>
     </body>
 
 </html>
