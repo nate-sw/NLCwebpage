78623b94efcb7b7bc88b93b0cb0caf78ce53e156
nate-sw <105872836+nate-sw@users.noreply.github.com>
2022-10-27 13:48:58 -0400

    Update midterm.php

--- midterm.php
+++ midterm.php
@@ -1,8 +1,21 @@
 <html>
 <body>
 
+
 <h1>midterm</h1>
-<?php if (isset($ppe) && $ppe =="masks") echo "cool"?>
+<?php
+$server = "localhost";
+$username = "php";
+$password = "password";
+$database = "products";
+$conn = mysqli_connect($server, $username, $password, $database);
+
+// Check for successful connection
+if (!$conn) {
+  die("Connection failed: {mysqli_connect_error()}");
+}
+echo "Connected successfully";
+?>
 
 
 
