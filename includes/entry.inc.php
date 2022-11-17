94af0f459853d2e1a37eab183434ca19ffbd13a1
nate-sw <105872836+nate-sw@users.noreply.github.com>
2022-11-17 17:57:53 -0500

    Update entry.inc.php

--- includes/entry.inc.php
+++ includes/entry.inc.php
@@ -14,23 +14,5 @@
     $sql = "INSERT INTO train_list (location, train_type, rail_op, reporting_mark, loco_num, loco_builder, loco_model) VALUES ('$location', '$type', '$operator', '$reporting', $number, '$builder', '$model')";
     mysqli_query($conn, $sql);
 
-    $entry_echo = "SELECT * FROM train_list ORDER BY id DESC LIMIT 1;";
-    $echo_result = $conn->query($echo_result);
-
-    if ($echo_result->num_rows > 0) {
-        // output data of each row
-        while($row = $echo_result->fetch_assoc()) {
-          echo "id: " . $row["id"]."<br>".
-          " -Continent" . $row["location"]."<br>".
-            " -Train Type " . $row["train_type"]."<br>".
-            " -Rail Operator " . $row["rail_op"]."<br>".
-            " -Reporting Mark " . $row["reporting_mark"]."<br>".
-            " -Locomotive Number " . $row["loco_num"]."<br>".
-            " -Locomotive Builder " . $row["loco_builder"]."<br>".
-            " -Locomotive Model " . $row["loco_model"]."<br>";
-        }
-      } else {
-        echo "0 results";
-      }
 
     header("Location: ../list.php?entry=success");
