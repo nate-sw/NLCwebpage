04d401309fc8809859c4a69a96f0aaf87a36957c
nate-sw <105872836+nate-sw@users.noreply.github.com>
2022-12-21 16:00:33 -0500

    Update test.php

--- final/test.php
+++ final/test.php
@@ -7,35 +7,7 @@
     </script>
 
 <h1>test</h1>
-    <?php
-    
-    include_once './includes/final.inc.php';
-
-    // Check for successful connection
-    if (!$conn) {
-    die("Connection failed: {mysqli_connect_error()}");
-    }
-    echo "Connected successfully". "<br>";
-    
-    ?>
-
-    <?php
-        $sql = "SELECT * FROM final;";
-        $result = $conn->query($sql);
-    
-        if ($result->num_rows > 0) {
-            // output data of each row
-            while($row = $result->fetch_assoc()) {
-              echo "search_query: " . $row["query"]."<br>".
-              " -ip_addr: " . $row["ip_addr"]."<br>".;
-            }
-          } else {
-            echo "0 results";
-          }
-        
-
-
-    ?>
+   
 
 
 
