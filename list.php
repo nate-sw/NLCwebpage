76db08096afbf17902b6522a2bfac84e136ee365
nate-sw <105872836+nate-sw@users.noreply.github.com>
2022-12-21 00:42:10 -0500

    Update list.php

--- list.php
+++ list.php
@@ -3,7 +3,7 @@
     <head>
     <script>
         function input_reply(){
-           document.getElementByID("entry_msg").innerHTML = "Thank you for your entry.";
+           document.getElementByID("entry_msg").innerHTML = "Thank you for your entry";
         }
     </script>
         <title>NLC</title>
